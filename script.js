3f304b17649e49a7b1649755230d01cc80396bd2
Cihan <ylmz_cihan@outlook.com>
2022-02-27 22:31:36 +0300

    Added buttons and result counter

--- script.js
+++ script.js
@@ -20,32 +20,45 @@ function eval_winner(first_sign, second_sign) {
     if (first_sign === "scissors" && second_sign === "paper") return first_sign;
     if (first_sign === "paper" && second_sign === "scissors") return second_sign;
 }
-
+let aiPoints = 0;
+let humanPoints = 0;
 //make a function to emulate a round of play
-function roundOfPlay() {
+function roundOfPlay(sign) {
     //make the computer choose
     let computerChoice = computerPlay();
     console.log("Computer's choice: " + computerChoice);
 
     //make the human choose
-    let userChoice = prompt("Please enter your choice: ").toLowerCase();
-    console.log("Human's choice: " + userChoice);
+    let userChoice = sign;
+    //prompt("Please enter your choice: ").toLowerCase();
+    console.log("Human's choicea: " + userChoice);
 
     //use "eval" function to evaluate the winner
     let winner = eval_winner(computerChoice, userChoice);
 
     if(winner)
     {
-        if (winner === userChoice) console.log("Human won!");
-        else console.log("The machines won!");
+        if (winner === userChoice) humanPoints++;
+        else aiPoints++;
     }
     else console.log("Draw");
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        roundOfPlay();
+    results.textContent = `Machine: ${aiPoints} Human: ${humanPoints}`
+    if(aiPoints === 5) {
+        results.textContent = "Machines won!";
+        aiPoints = 0;
+        humanPoints = 0;
+    }
+    else if (humanPoints === 5) {
+        results.textContent = "Human won!";
+        aiPoints = 0;
+        humanPoints = 0;
     }
 }
 
-game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        roundOfPlay(button.id);
+    })
+});
+
